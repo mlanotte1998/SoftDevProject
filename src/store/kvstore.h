adf150fcbf7930d53820ca6dea0a8d860f91ff8b
welchdaniel <welch.da@husky.neu.edu>
2020-03-23 15:01:13 -0400

    updated KVStore

--- src/store/kvstore.h
+++ src/store/kvstore.h
@@ -6,6 +6,14 @@ class KVStore {
 public:
     Map* map_;
 
+    KVStore() {
+        map_ = new Map();
+    }
+
+    ~KVStore() {
+        delete map_;
+    }
+
     Value* get(Key key) {
         return dynamic_cast<Value*>(map_->get(&key));
     }
@@ -14,7 +22,7 @@ public:
         return dynamic_cast<Value*>(map_->get(&key));
     }
 
-    void put(Key key, Value value) {
-        map_->put(&key, &value);
+    void put(Key key, Value* value) {
+        map_->put(&key, value);
     }
 };
