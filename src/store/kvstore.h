88574f1dc7554cef1e5fd0c902563cefe1a4925d
welchdaniel <welch.da@husky.neu.edu>
2020-03-23 13:05:48 -0400

    implemented basic kvstore

--- src/store/kvstore.h
+++ src/store/kvstore.h
@@ -1,17 +1,20 @@
 #include "key.h"
 #include "value.h"
+#include "../utility-classes/map.h"
 
 class KVStore {
 public:
-    Value* get(Key key) {
+    Map* map_;
 
+    Value* get(Key key) {
+        return dynamic_cast<Value*>(map_->get(&key));
     }
 
     Value* waitAndGet(Key key) {
-
+        return dynamic_cast<Value*>(map_->get(&key));
     }
 
     void put(Key key, Value value) {
-
+        map_->put(&key, &value);
     }
 };
