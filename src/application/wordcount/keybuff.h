9b72f0139fd36f128377d55980918ea9ca6a7a50
welchdaniel <welch.da@husky.neu.edu>
2020-04-16 10:31:15 -0400

    added Vitek's Key, String, KeyBuff, StrBuff code

--- src/application/wordcount/keybuff.h
+++ src/application/wordcount/keybuff.h
@@ -2,22 +2,25 @@
 #include "../../utility-classes/string.h"
 
 /* author: jv */
-class KeyBuff : public Object {                                                  
-  public:                                                                        
-  Key* orig_; // external                                                        
-  StrBuff buf_;                                                                  
-                                                                                 
-  KeyBuff(Key* orig) : orig_(orig), buf_() {};                      
-                                                                                 
-  KeyBuff& c(String &s) { buf_.c(s); return *this;  }                            
-  KeyBuff& c(size_t v) { buf_.c(v); return *this; }                              
-  KeyBuff& c(const char* v) { buf_.c(v); return *this; }                         
-                                                                                 
-  Key* get() {                                                                   
-    String* s = buf_.get();                                                      
-    buf_.c(orig_->c_str());                                                      
-    Key* k = new Key(s->steal(), orig_->get_idx());                                 
-    delete s;                                                                    
-    return k;                                                                    
-  }                                                                              
+class KeyBuff : public Object {
+  public:
+  Key* orig_; // external
+  StrBuff buf_;
+
+
+  KeyBuff(Key* orig) : orig_(orig), buf_(*orig) {}
+  KeyBuff(const char* name) : orig_(new Key("ignore")), buf_(name) {}
+  
+  KeyBuff& c(String &s) { buf_.c(s); return *this;  }
+  KeyBuff& c(size_t v) { buf_.c(v); return *this; }
+  KeyBuff& c(const char* v) { buf_.c(v); return *this; }
+
+
+  Key* get() {   
+    String* s = buf_.get();
+    buf_.c(orig_->c_str());
+    Key* k = new Key(s->steal(), orig_->home());
+    delete s;
+    return k;
+  } 
 }; // KeyBuff
--- src/store/key.h
+++ src/store/key.h
@@ -1,48 +1,62 @@
 #pragma once
 
 #include "../utility-classes/object.h"
+#include "../utility-classes/array.h"
 
-/***
-* Key::
-*
-* A Key is an object designed to be used in a key-value store (KVStore, KDStore).
-* Every key object has a name and index.
-* authors: welch.da@husky.neu.edu, lanotte.m@husky.neu.edu
-*/
-class Key : public Object {
-	public:
-		const char* name_;
-		size_t idx_;
+/*****************************************************************************
+ * A Key is represented by a string value and the index of a home node.
+ *************************************************   author: vitekj@me.com */
+class Key : public String {
+public:
+  size_t home_;  // index of the home node
 
-		/** Key constructor */
-		Key(const char* name, size_t idx) {
-			name_ = name;
-			idx_ = idx;
-		}
+  /** Create a key from a c-string; use the hash value to pick the node */
+  Key(char const* cstr) : String(cstr, strlen(cstr)) {
+    home_ = hash() % arg.num_nodes;
+  }
 
-		/** Key constructor no index */
-		Key(const char* name) {
-			name_ = name;
-			idx_ = 0;
-		}
+  Key(Key & from):
+    String(from),
+    home_(from.home_) {
+  }
 
-		/** Method for calculating Key equality */
-		bool equals(Key other) {
-			return name_ == other.name_ && idx_ == other.idx_;
-		}
+  /** Create a key from a c-string; explicitly checks the home node */
+  Key(char const* cstr, size_t home) : String(cstr, strlen(cstr)) {
+    home_ = home;
+  }
 
-		/** Returns the index of this Key */
-		size_t get_idx() {
-			return idx_;
-		}
+  /** Create a key from a String; use its hash value to pick the node */    
+  Key(String& from) : String(from) { home_ = hash() % arg.num_nodes; }
+  
+  /** Create a key from a String; explicitly choose the node*/
+  Key(String& from, size_t home) : String(from) { home_ = home; }
 
-		/** Method for hashing a Key */
-		size_t hash_me() {
-			size_t hash = 0;
-			for (size_t i = 0; i < strlen(name_); ++i) {
-				hash = name_[i] + (hash << 6) + (hash << 16) - hash;
-			}
-			hash += idx_;
-			return hash;
-		}
-};
+	Key(String * from):
+		String(true, from->steal()) {
+		home_ = hash() % arg.num_nodes;
+	}
+    
+  /** Create a key from a String; consumes and deletes the String */
+  Key(bool steal, String* from, size_t home) :
+    String(steal, from->steal(), from->size()) {
+    home_ = home;
+    delete from;
+  }
+
+  Key(char const * start, size_t length, size_t home):
+    String(start, length) {
+    home_ = home;
+  }
+
+  /** Copy the key */
+  Key *clone() override { return new Key(*this, home_); }
+  
+  /** Return the index of the node where the key is homed. */
+  size_t home() { return home_; }
+
+  bool equals(Object * other) override {
+    Key * k2 = dynamic_cast<Key *>(other);
+    if (k2 == nullptr || k2->home_ != home_) return false;
+    return String::equals(other);  
+  }
+}; // Key
