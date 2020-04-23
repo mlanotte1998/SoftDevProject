d616636db4ae05b9b96596f585f7021e64cd6763
mlanotte1998 <mlanotte1998@gmail.com>
2020-04-23 10:11:53 -0400

    adding test

---
+++ test-error.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Michael Lanotte on 4/23/20.
+//
+
+int main(int argc, char* argv[]) {
+
+    size_t zero = 0;
+    KeyBuff* kbuff = new KeyBuff(new Key("wc-map-", 0));
+
+    Key * k = kbuf->c(zero).get();
+
+    return 0;
+
+}
+
